3e5e3b0ebc73dca8cbd7832f542a0d33ad61f9c2
Alexander Lewis <alex.lewis79@yahoo.co.uk>
2015-12-20 23:28:44 +0000

    more crud

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,9 +9,52 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      flash[:notice] = "Your new post was succesfully created"
+      redirect_to @post
+    else
+      flash[:error] = "There was a problem saving your post. Please try again!"
+      render :new
+    end
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
+
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update(post_params)
+      flash[:notice] = "Your post was succesfully updated!"
+      redirect_to @post
+    else
+      flash[:error] = "There was an error updating your post. Please try again!"
+      render :edit
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+
+    if @post.destroy
+      flash[:notice] = "Your post was succesfully deleted!"
+      redirect_to @posts
+    else
+      flash[:error] = "There was an error deleting your post. Please try again!"
+      redirect_to @post
+    end
+  end
+
+  private
+
+    def post_params
+      params.require(:post).permit(:title, :body)
+    end
 
 end

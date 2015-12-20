aea21076cd46899b306be7fd64f6d94917c0e14c
Alexander Lewis <alex.lewis79@yahoo.co.uk>
2015-12-20 00:34:35 +0000

    crud finished

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
+
+  resources :posts
+  get "about" => "welcome#about"
+  get "contact" => "welcome#contact"
   root to: 'welcome#index'
-
-  get 'welcome/about'
-  get 'welcome/contact'
-
 
 end

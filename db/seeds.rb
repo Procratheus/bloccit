d94a9b3e00e4a993675ff73002762c43d5d65d43
Alexander Lewis <alex.lewis79@yahoo.co.uk>
2015-12-20 00:08:40 +0000

    completed seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,51 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'faker'
+
+Post.destroy_all
+Comment.destroy_all
+Question.destroy_all
+Answer.destroy_all
+
+50.times do
+  post = Post.new(
+    title: Faker::Lorem::sentence,
+    body: Faker::Lorem::paragraph
+  )
+  post.save
+end
+
+posts = Post.all
+
+100.times do
+  comment = Comment.new(
+    post: posts.sample,
+    body: Faker::Lorem::paragraph
+  )
+  comment.save
+end
+
+comments = Comment.all
+
+50.times do
+  question = Question.new(
+    title: Faker::Lorem::sentence,
+    body: Faker::Lorem::paragraph
+  )
+  question.save
+end
+
+questions = Question.all
+
+100.times do
+  answer = Answer.new(
+    question: questions.sample,
+    body: Faker::Lorem::paragraph
+  )
+  answer.save
+end
+
+answers = Answer.all
+
+puts "#{posts.count} posts created."
+puts "#{comments.count} comments created."
+puts "#{questions.count} questions created."
+puts "#{answers.count} answer created."

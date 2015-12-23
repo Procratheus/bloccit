0435fa6a0087f789c6d95428ae0b929711661e33
Alexander Lewis <alex.lewis79@yahoo.co.uk>
2015-12-23 23:40:45 +0000

    finished topics

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,21 +1,26 @@
 class PostsController < ApplicationController
 
   def show
+    @topic = Topic.find(params[:topic_id])
     @post = Post.find(params[:id])
     authorize @post
   end
 
   def new
+    @topic = Topic.find(params[:topic_id])
     @post = Post.new
     authorize @post
   end
 
   def create
-    @post = current_user.posts.new(post_params)
+    @topic = Topic.find(params[:topic_id])
+    @post = Post.new(post_params)
+    @post.topic = @topic
+    @post.user = current_user
     authorize @post
     if @post.save
       flash[:notice] = "Your new post was succesfully created"
-      redirect_to @post
+      redirect_to [@topic, @post]
     else
       flash[:error] = "There was a problem saving your post. Please try again!"
       render :new
@@ -23,17 +28,19 @@ class PostsController < ApplicationController
   end
 
   def edit
+    @topic = Topic.find(params[:topic_id])
     @post = Post.find(params[:id])
     authorize @post
   end
 
   def update
+    @topic = Topic.find(params[:topic_id])
     @post = Post.find(params[:id])
     authorize @post
 
     if @post.update(post_params)
       flash[:notice] = "Your post was succesfully updated!"
-      redirect_to @post
+      redirect_to [@topic, @post]
     else
       flash[:error] = "There was an error updating your post. Please try again!"
       render :edit
@@ -41,12 +48,13 @@ class PostsController < ApplicationController
   end
 
   def destroy
+    @topic = Topic.find(params[:topic_id])
     @post = Post.find(params[:id])
     authorize @post
 
     if @post.destroy
       flash[:notice] = "Your post was succesfully deleted!"
-      redirect_to @posts
+      redirect_to @topic
     else
       flash[:error] = "There was an error deleting your post. Please try again!"
       redirect_to @post

157dcf0022fd3f3d9934aae17d8d2cdb48c39e75
Alexander Lewis <alex.lewis79@yahoo.co.uk>
2015-12-24 00:18:05 +0000

    interlude

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,18 @@
+class UsersController < ApplicationController
+
+  def update
+    if current_user.update(user_params)
+      flash[:notice] = "User updated"
+      redirect_to edit_user_registration_path
+    else
+      flash[:error] = "There was a problem updating your details. Please try again!"
+      redirect_to edit_user_registration_path
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name)
+    end
+end

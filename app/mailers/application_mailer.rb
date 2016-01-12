4cb27321db1f578c9659ca531d96dbf92ef86f11
Alexander Lewis <alex.lewis79@yahoo.co.uk>
2016-01-12 12:38:08 +0000

    action mailer

---
+++ app/mailers/application_mailer.rb
@@ -0,0 +1,4 @@
+class ApplicationMailer < ActionMailer::Base
+  default from: "alex.lewis79@yahoo.co.uk"
+  layout 'mailer'
+end
---
+++ app/mailers/favorite_mailer.rb
@@ -0,0 +1,16 @@
+class FavoriteMailer < ApplicationMailer
+
+  def new_comment(user, post, comment)
+
+    ## New headers
+    headers['Message-ID'] = '<comments/#{comment.id}@fierce-lake-9253.herokuapp.com>'
+    headers['In-Reply-To'] = '<post/#{post.id}@fierce-lake-9253.herokuapp.com>'
+    headers['References'] = '<post/#{post.id}@fierce-lake-9253.herokuapp.com>'
+
+    @user = user
+    @post = post
+    @comment = comment
+
+    mail(to: user.email, subject: "New comment on #{post.title}")
+  end
+end

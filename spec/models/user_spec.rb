dfbccf6df0f85944d2e3b53e094649d5f165a490
Alexander Lewis <alex.lewis79@yahoo.co.uk>
2016-01-12 13:10:35 +0000

    completed favorites

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,13 +4,19 @@ describe User do
 
   include TestFactories
 
+  before do
+    @user = authenticated_user
+    @post = associated_post
+  end
+
   describe "#favourited(post)" do
     it "returns nil if a user has not favourited a post" do
-
+      expect( @user.favorited(@post) ).to eq(nil)
     end
 
     it "returns the relevant post if it has been favourited" do
-
+      favorite = @user.favorites.where(post_id: @post).create
+      expect( @user.favorited(@post) ).to eq(favorite)
     end
   end
 end

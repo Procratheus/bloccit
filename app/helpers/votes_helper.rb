ce9d0aea8926c14371e9d08d47ab725edc780e30
Alexander Lewis <alex.lewis79@yahoo.co.uk>
2016-01-16 22:39:05 +0000

    completed

--- app/helpers/votes_helper.rb
+++ app/helpers/votes_helper.rb
@@ -1,2 +1,11 @@
 module VotesHelper
+
+  def upvote_link_classes(post)
+    current_user.voted(post).upvote? ? 'voted' : ''
+  end
+
+  def downvote_link_classes(post)
+    current_user.voted(post).downvote? ? 'voted' : ''
+  end
+
 end

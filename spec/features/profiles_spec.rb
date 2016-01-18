d108bbbdee29a503e516bcbe6ec6dffb4b40b30b
Alexander Lewis <alex.lewis79@yahoo.co.uk>
2016-01-18 23:15:43 +0000

    warden test

--- spec/features/profiles_spec.rb
+++ spec/features/profiles_spec.rb
@@ -10,11 +10,21 @@ describe "Proifile Page" do
     @comment = Comment.new(body: "This is a new comment", post: @post, user: @user)
     allow(@comment).to receive(:send_favorite_emails)
     @comment.save
+    @signed_in_user = login_as(@user, scope: :user)
   end
 
   describe "not signed in" do
     it "should allow the visitor to view the profile" do
       visit user_path(@user)
+
+      expect(current_path).to eq(user_path(@user))
+      expect( page ).to have_content( @user.name )
+      expect( page ).to have_content( @post.title )
+      expect( page ).to have_content( @comment.body )
+    end
+
+    it "should allow a user to visit someone elses profile" do
+      visit user_path(@signed_in_user)
 
       expect(current_path).to eq(user_path(@user))
       expect( page ).to have_content( @user.name )
